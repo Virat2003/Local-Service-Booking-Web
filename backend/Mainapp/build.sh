445f8cb1994c692c9208beda17e7b239dc070e8b
Virat2003 <viratrane2616@gmail.com>
2026-02-21 22:56:43 +0530

    testing superuser

--- backend/Mainapp/build.sh
+++ backend/Mainapp/build.sh
@@ -5,3 +5,11 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
+python manage.py shell << END
+from account.models import User
+user = User.objects.get(username="viratadmin")
+user.set_password("viratadmin@11")
+user.is_staff = True
+user.is_superuser = True
+user.save()
+END
