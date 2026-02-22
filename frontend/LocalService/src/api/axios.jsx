ca4f27331127b8932cd2cd25b9d33f1a0e4175d7
Virat2003 <viratrane2616@gmail.com>
2026-02-22 11:21:40 +0530

    CORS ALLOWED ORIGIN

--- frontend/LocalService/src/api/axios.jsx
+++ frontend/LocalService/src/api/axios.jsx
@@ -1,7 +1,14 @@
 import axios from "axios";
 
+// const api = axios.create({
+//   baseURL: "http://127.0.0.1:8000/api/",
+// });
+
+const isDevelopment = import.meta.env.MODE === 'development'
+const myBaseUrl = isDevelopment ? import.meta.env.VITE_API_BASE_URL_LOCAL : import.meta.env.VITE_API_BASE_URL_DEPLOY
+
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: myBaseUrl,
 });
 
 // Attach token automatically
